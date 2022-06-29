036d0bd707b913d18d2957a176433ec720adc129
Илья Колесниченко <iliakoll@yandex.ru>
2022-06-29 22:16:54 +0300

    Оформил импорты по PEP8

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -9,10 +9,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = (
-            'id', 'username', 'first_name', 'last_name',
-            'posts', 'comments'
-        )
+        fields = '__all__'
         ref_name = 'ReadOnlyUsers'
 
 
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,10 +1,11 @@
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import get_object_or_404
-from posts.models import Group, Post, User
 from rest_framework import filters, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import (IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
+
+from posts.models import Group, Post, User
 
 from .mixins import CreateGetListViewSet
 from .permissions import AuthorOrReadOnly
